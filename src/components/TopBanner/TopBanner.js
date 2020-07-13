24af8c52e21bcbf259fa90dec39d140a71c87b0a
Masud Rana <62185541+M-Rana-Info@users.noreply.github.com>
2020-07-13 21:48:15 +0600

    v.1 complete
    v.1 complete

--- src/components/TopBanner/TopBanner.js
+++ src/components/TopBanner/TopBanner.js
@@ -9,7 +9,7 @@ class TopBanner extends Component {
                         <Container className="topContent">
                             <Row>
                                 <Col className="text-center">
-                                    <h1 className="topTitle">FREELANCER MASUD RANA</h1>
+                                    <h1 className="topTitle">MASUD RANA</h1>
                                     <h4 className="topSubTitle">Full Stack Web Developer</h4>
                                     <Button variant="primary">More Info</Button>
                                 </Col>
